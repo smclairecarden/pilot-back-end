e8fcf6001556b70af7a5c7dca798ee491b8b2549
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-03-25 14:11:30 -0500

    add comment schema

--- models/location.js
+++ models/location.js
@@ -2,10 +2,24 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema ({
+  rating: {
+    type: Number, 
+    min: 1, 
+    max: 5, 
+    required: true
+  },
+  content: {type: String, required: true},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
 const locationSchema = new Schema({
   name: {type: String, required: true},
   description: {type: String},
-  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  comments: [commentSchema]
 }, {
   timestamps: true
 })
