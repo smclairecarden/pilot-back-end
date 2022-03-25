721a58e0ef09a097d5ef08c0213d3063ab907b1a
Vanessa Konynenbelt <konyn005@umn.edu>
2022-03-25 14:24:37 -0500

    customize profile schema

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,30 @@
 import mongoose from 'mongoose'
 
+const Schema = mongoose.Schema
+
+//embedded pilot Schema
+const pilotSchema = new Schema ({
+  rating: {
+    type: Number, 
+    min: 1, 
+    max: 10, 
+    required: true
+  },
+  content: {type: String, required: true},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  location: String, 
+  skillLevel: Number, 
+  pilot: [pilotSchema], 
+  // photo: {type: String}, TODO
+  favLocations: {type: mongoose.Schema.Types.ObjectId, ref: "Location"}, 
+  // messages: {type: mongoose.Schema.Types.ObjectId, ref: "Message"}, TODO
 },{
     timestamps: true,
 })
