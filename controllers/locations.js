749cd2870cc8c6dfe9784b64f1b60429fed75c65
Claire Carden <smclairecarden@gmail.com>
2022-03-25 12:20:23 -0500

    add change

--- controllers/locations.js
+++ controllers/locations.js
@@ -4,21 +4,33 @@ function index (req, res) {
   Location.find({})
   .populate('owner')
   .then(locations => {
-    res.json(locations)
+    return res.json(locations)
   })
   .catch(err => {
     res.json(err)
   })
 }
 
-function create(req, res) {
-  req.body.owner = req.user.profile
-  Location.create(req.body)
-  .then(location => {
-    location.populate('owner')
-    .then(populateLocation)
-  })
-  .catch(err => res.json(err))
+async function create(req, res) {
+  try{
+    req.body.owner = req.user.profile
+    const location = new Location(req.body)
+    await location.save()
+    return res.status(201).json(location)
+  } catch(err) {
+    return res.status(500).json(err)
+  }
+
+  
+  // Location.create(req.body)
+  //   .populate('owner')
+  //   .exec(newLocation => {
+  //     return 
+  //   })
+  // .catch(err => {
+  //   console.log(err)
+    
+  // })
 }
 
 
