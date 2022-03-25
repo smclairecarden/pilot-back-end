3743b9e6e5f7ebeacce0769cc7ebb2c73d35e944
Claire Carden <smclairecarden@gmail.com>
2022-03-25 10:40:53 -0500

    edit location router

--- routes/locations.js
+++ routes/locations.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import * as locationsCtrl from '../controllers/locations.js'
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
-import { router } from './profiles.js'
+
+const router = Router()
 
 // router.get('/:id', locationsCtrl.show)
 router.get('/', locationsCtrl.index)
