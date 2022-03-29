e21d40ec7add260f288f1b4f5dcdbfd038205cc3
Claire Carden <smclairecarden@gmail.com>
2022-03-29 15:27:42 -0500

    delete comment work

--- routes/locations.js
+++ routes/locations.js
@@ -6,14 +6,14 @@ const router = Router()
 
 // router.get('/:id', locationsCtrl.show)
 router.get('/', locationsCtrl.index)
-router.get('/:name', locationsCtrl.getLocation)
+// router.get('/:name', locationsCtrl.getLocation)
 router.get('/:id/comments', locationsCtrl.show)
 
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, locationsCtrl.create)
 router.post('/:id/comments', locationsCtrl.createComment)
 router.put('/:id', checkAuth, locationsCtrl.update)
-router.delete('/:id', checkAuth, locationsCtrl.delete)
+router.delete('/:locationId/comments/:commentId', checkAuth, locationsCtrl.delete)
 
 
 
