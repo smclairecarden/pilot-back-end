a52c863201777739bdaf4dce6d8b7b54e512fe35
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-03-29 19:24:06 -0500

    more profile work

--- routes/profiles.js
+++ routes/profiles.js
@@ -11,7 +11,7 @@ const router = Router()
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 
-router.post('/', profilesCtrl.createProfile)
+router.post('/', checkAuth, profilesCtrl.create)
 
 
 export { router }
