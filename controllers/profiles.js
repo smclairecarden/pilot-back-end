250a4de0b120ec8ff97bdb84fd3137b78d7d911f
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-03-29 19:00:46 -0500

    write create profile ctrl function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,5 +1,5 @@
 import { Profile } from '../models/profile.js'
-import axios from 'axios'
+// import axios from 'axios'
 import {v2 as cloudinary} from 'cloudinary'
 
 function index(req, res) {
@@ -12,11 +12,41 @@ function index(req, res) {
 }
 
 function createProfile(req, res) {
-  
-
+  req.body.owner = req.user.profile
+  if (req.body.photo === 'undefined' || !req.files['photo']) {
+    delete req.body['photo']
+    Profile.create(req.body)
+    .then(profile => {
+      profile.populate('owner')
+      .then(populatedProfile => {
+        res.status(201).json(populatedProfile)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  } else {
+    const imageFile = req.files.photo.path
+    cloudinary.uploader.upload(imageFile, {tags: `${req.body.name}`})
+    .then(photo => {
+      req.body.photo = photo.url
+      Profile.create(req.body)
+      .then(profile => {
+        profile.populate('owner')
+        .then(populatedProfile => {
+          res.status(201).json(populatedProfile)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+    })
+  }
 }
 
 export { 
   index,
   createProfile as create,
- }
+}
